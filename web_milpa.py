2851a95370e8960271294c2ef0eb23888218ec6c
yadlra <yadi.sb1@gmail.com>
2022-06-01 14:05:19 +0200

    more

--- web_milpa.py
+++ web_milpa.py
@@ -10,66 +10,65 @@ app = Flask(__name__)
 socket_ = SocketIO(app, async_mode=async_mode)
 
 
-# def template(title = "HELLO!", text = ""):
-#     now = datetime.datetime.now()
-#     timeString = now
-#     templateDate = {
-#         'title' : title,
-#         'time' : timeString,
-#         'text' : text
-#         }
-#     return templateDate
+def template(title = "HELLO!", text = ""):
+    now = datetime.datetime.now()
+    timeString = now
+    templateDate = {
+        'title' : title,
+        'time' : timeString,
+        'text' : text
+        }
+    return templateDate
+
+def compare(s):
+    m = "there was an error"
+    if s > -1.0:
+        if s < 200:
+            m = "200"
+        elif s < 300:
+            m = "300"
+        elif s < 500:
+            print(s)
+            m = "500"
+        elif s < 700:
+            print(s)
+            m = "700"
+        else:
+            print(s)
+            m = "> 700"
+    
     
 
 @app.route("/")
 def index():
     # templateData = template()
     # return render_template('main.html', **templateData)
-    return render_template('index.html',
+    
+    message = "unknown"
+    status = -1.0
+
+    with serial.Serial("/dev/ttyACM0", 9600, timeout=1) as arduino:
+        time.sleep(0.1)
+        if arduino.isOpen():
+            print("{} connected!".format(arduino.port))
+            try:
+                while True:
+                    val = arduino.readline()
+                    print(val)
+                    arduino.flushInput()
+                    status = struct.unpack('<f', val)
+                    message = compare(status)
+                    
+                    return render_template('index.html',
                            sync_mode=socket_.async_mode)
 
+    
 
-# @app.route("/sensor")
-# def action():
-#     message = "unknown"
-#     status = -1.0
+    templateData = template(text = message)
+    # return render_template('main.html', **templateData)
 
-#     with serial.Serial("/dev/ttyACM0", 9600, timeout=1) as arduino:
-#         time.sleep(0.1)
-#         if arduino.isOpen():
-#             print("{} connected!".format(arduino.port))
-#             try:
-#                 while True:
-#                     val = arduino.readline()
-#                     print(val)
-#                     arduino.flushInput()
-#                     status = struct.unpack('<f', val)
-#             except:
-#                 status = -1.0
-
-#     if status > -1.0:
-#         if status < 200:
-#             message = "200"
-#         elif status < 300:
-#             message = "300"
-#         elif status < 500:
-#             print(status)
-#             message = "500"
-#         elif status < 700:
-#             print(status)
-#             message = "700"
-#         else:
-#             print(status)
-#             message = "> 700"
-#     else:
-#         message = "There was an error"
-
-#     templateData = template(text = message)
-#     # return render_template('main.html', **templateData)
     
 
 if __name__ == "__main__":
-    # socket_.run(app, debug=True)
     socket_.run(app, host='0.0.0.0', debug=True)
-    # app.run(host='0.0.0.0')
-    # # app.run(port=5000, host='0.0.0.0', debug=True)
+   
