f15d1a36c1b98b3d8e0dcaf0b7c541e4b42314ca
yadlra <yadi.sb1@gmail.com>
2022-06-01 16:27:56 +0100

    c

--- web_milpa.py
+++ web_milpa.py
@@ -1,15 +1,18 @@
+
 from flask import Flask, render_template
-from flask_socketio import SocketIO
+from flask_socketio import SocketIO, emit
 import serial
 import struct
 import time
 from threading import Lock
 
+arduino = None
 async_mode = None
 app = Flask(__name__)
 socket_ = SocketIO(app, async_mode=async_mode)
 thread = None
 thread_lock = Lock()
+
 message = "Waiting for data"
 status = -1.0
 
@@ -23,7 +26,7 @@ def template(title="HELLO!", text=""):
 def compare(s):
     print(s)
     m = "there was an error"
-    if s > -1.0:  
+    if s > -1.0:
         if s < 200:
             m = "200"
         elif s < 300:
@@ -41,23 +44,31 @@ def compare(s):
 
 @app.route("/")
 def index():
-    # templateData = template(text = message)
-    # return render_template('main.html', **templateData)
     return render_template('index.html', sync_mode=socket_.async_mode)
+
+@socket_.on('openSerial', namespace='/sensor')
+def openSerial():
+    # Open the serial port to the arduino
+    arduino = serial.Serial("/dev/ttyACM0", 9600, timeout=1)
+    if arduino.isOpen():
+        print("{} connected!".format(arduino.port))
+        emit('serialOpen')
 
 @socket_.on('readSensor', namespace='/sensor')
 def readSerial():
-    with serial.Serial("/dev/ttyACM0", 9600, timeout=1) as arduino:
-        time.sleep(0.1)
-        if arduino.isOpen():
-            print("{} connected!".format(arduino.port))
-            incoming = arduino.readline().decode('ascii').strip()
-            if not incoming is "":
-            	print(incoming)
-            	arduino.flushInput()
-            	status = float(incoming)
-            	message = compare(status)
-            	socket_.emit('sensorData', { "data": message })
+    if arduino != None:
+        incoming = arduino.readline().decode('ascii').strip()
+        print("{} incoming".format(incoming))
+        if incoming != "":
+            print(incoming)
+            arduino.flushInput()
+            status = float(incoming)
+            message = compare(status)
+            print('emitting data')
+            emit('sensorData', { "data": message })
+        else:
+            arduino.flushInput()
+            print("incoming was empty")
 
 if __name__ == "__main__":
     readSerial()
